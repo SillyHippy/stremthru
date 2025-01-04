ead6021e69ad1e9d6b06e502adf562fc6ed289d0
SillyHippy <159052837+SillyHippy@users.noreply.github.com>
2025-01-04 13:18:03 -0600

    Update index.go

--- api/index.go
+++ api/index.go
@@ -5,13 +5,11 @@ import (
 	"net/http"
 )
 
-// handler handles HTTP requests and sends a response.
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Vercel! Your request path is: %s", r.URL.Path)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	// Start the server on port 8080 (required by Vercel's serverless runtime).
 	http.ListenAndServe(":8080", nil)
 }
